bb3dfe5b30c445010552e04e114f03538a371f55
Trea Hauet <trea@treahauet.com>
2023-02-19 14:09:31 -0700

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4NTUwNg/spectral.js?branch_slug=main&token=d1932122-ddc0-46d1-88b1-a064e3135ed0";
+export default { extends: ruleset };
